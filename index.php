0a36ffca8f85892a24882d786b49bc83a2205810
wellingtonsilvaj <72203898+wellingtonsilvaj@users.noreply.github.com>
2023-01-18 19:17:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,42 +3,42 @@
 //carrega um usuario
 
 require_once("config.php");
-/*$root = new Usuario();
+*$root = new Usuario();
 $root->loadbyId(5);
 echo $root;*/
 
-//carrega uma lista de usuarios
+carrega uma lista de usuarios
 
-/*$lista = Usuario::getList();
+$lista = Usuario::getList();
 echo json_encode($lista);*/
 
-//carrega uma lista de usuarios buscando pelo login
+carrega uma lista de usuarios buscando pelo login
 
-/*$search = Usuario::search("ma");
+$search = Usuario::search("ma");
 echo json_encode($search);*/
 
-//carrega um usuario usando o login e a senha
+carrega um usuario usando o login e a senha
 
-/*$usuario = new Usuario();
+$usuario = new Usuario();
 $usuario->login("carlos"," 123345656");
 echo $usuario;*/
 
-//criando um novo usuario
+criando um novo usuario
 
-/*$aluno = new Usuario("","" );
+$aluno = new Usuario("","" );
 
 $aluno->insert();
 
 echo $aluno;*/
 
-//alterando usuario
-/*$usuario = new Usuario();
+alterando usuario
+$usuario = new Usuario();
 
 $usuario->loadById(3);
 $usuario->update("ale", "245868");
 echo $usuario;*/
 
-//apagando usuario
+apagando usuario
 
 $usuario = new Usuario();
 
